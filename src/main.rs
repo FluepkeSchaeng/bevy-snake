094eb6cd0ad9bfba9ca1f03f0e9b27fd0f0c96da
Philipp Pickartz <pickartzphilipp@googlemail.com>
2023-03-25 02:44:12 +0100

    add default 2d window

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use bevy::prelude::*;
 
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_startup_system(setup_camera)
+        .add_plugins(DefaultPlugins)
+        .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
