00bc07fc8d256d23c141abafd400ee7f816eca50
Philipp Pickartz <pickartzphilipp@googlemail.com>
2023-03-26 10:50:04 +0200

    using the grid

--- src/main.rs
+++ src/main.rs
@@ -63,20 +63,20 @@ fn spawn_snake(mut commands: Commands) {
 
 fn snake_movement(
     keyboard_input: Res<Input<KeyCode>>,
-    mut head_positions: Query<(&mut Transform, With<SnakeHead>)>,
+    mut head_positions: Query<(&mut Position, With<SnakeHead>)>,
 ) {
-    for (mut transform, _) in head_positions.iter_mut() {
+    for (mut pos, _) in head_positions.iter_mut() {
         if keyboard_input.pressed(KeyCode::Left) {
-            transform.translation.x -= 2.0;
+            pos.x -= 1;
         }
         if keyboard_input.pressed(KeyCode::Right) {
-            transform.translation.x += 2.0;
+            pos.x += 1;
         }
         if keyboard_input.pressed(KeyCode::Down) {
-            transform.translation.y -= 2.0;
+            pos.y -= 1;
         }
         if keyboard_input.pressed(KeyCode::Up) {
-            transform.translation.y += 2.0;
+            pos.y += 1;
         }
     }
 }
